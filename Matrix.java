ee534de34022280edf7aa6f21c52de55b2467f29
Alison Gim <AlisonGim@Dorothys-MacBook-Air.local>
2018-01-05 22:04:52 -0500

    Elementary Row Ops and Row Echelon Form

--- Matrix.java
+++ Matrix.java
@@ -2,24 +2,28 @@ public class Matrix
 {
 	int rows;
 	int cols;
-	int vals[];
-	int matrix[][];
+	float vals[];
+	float matrix[][];
+
+	/* Default constructor: greats a 2 x 2 matrix filled with 0s */
 	public Matrix()
 	{
 		rows = 2;
 		cols = 2;
-		vals =  new int[4];
-		matrix = new int[2][2];
+		vals =  new float[4];
+		matrix = new float[2][2];
 	}
 	
-	public Matrix(int r, int c, int v[])
+	/* Takes row dimension r, column dimension c, and an array of float values */
+	public Matrix(int r, int c, float v[])
 	{
 		rows = r;
 		cols = c;
 		vals = v;
-		matrix = new int[r][c];
+		matrix = new float[r][c];
 	}
 	
+	/* Prints matrix in terminal  */
 	public void printMatrix()
 	{
 		String ret = "";
@@ -29,11 +33,13 @@ public class Matrix
 			{
 				ret = ret + matrix[i][j] + "\t";
 			}
-			if(i + 1 != rows) ret += "\n";
+			// if(i + 1 != rows) ret += "\n";
+			ret += "\n";
 		}
 		System.out.println(ret);
 	}
-
+		
+	/*  If dimensions of the matrix matches the number of values, fills the matrix with the values. Otherwise, prints and error message */
 	public void fillMatrix()
 	{
 		if(vals.length != rows * cols)
@@ -52,7 +58,82 @@ public class Matrix
 			}
 		}
 	}
+	
+	/* **Elementary Row Operations** */
 
+	/* Switch row x with row y */
+	public void switchRows(int x, int y)
+	{
+		for(int i = 0; i < cols; i++)
+		{
+			float temp = matrix[x][i];
+			matrix[x][i] = matrix[y][i];
+			matrix[y][i] = temp;
+		}
+	}
+	
+	/* Multiply row x by y */
+	public void multiplyRow(int x, float y)
+	{
+		if(y == 0)
+		{
+			System.out.println("You must multiply by a non-zero number");
+			return;
+		}
+		for(int i = 0; i < cols; i++)
+		{
+			matrix[x][i] *= y;
+		}
+	}
+
+	/* Add product of z and row y to x */
+	public void addRowMultiple(int x, int y, float z)
+	{
+		for(int i = 0; i < cols; i++)
+		{
+			matrix[x][i] = matrix[x][i] + z*matrix[y][i];
+		}
+	}
+
+	public void rowEchelon()
+	{
+		for(int j = 0, i = 0; j < cols && i < rows; j++, i++)
+		{
+			int count = rows - 1;
+			// Put 0 rows on the bottom of the matrix
+			for(int k = i; k < rows; k++)
+			{
+				if(matrix[k][j] == 0)
+				{
+					while(matrix[count][j] == 0) count--;
+					if(matrix[count][j] != 0 && count > k)
+					{
+						switchRows(k, count);
+					}
+				}
+			}
+			// Creates leading rows
+			for(int k = i; k < rows && matrix[k][j] != 0; k++)
+			{
+				multiplyRow(k, (float)(Math.pow(matrix[k][j], -1)));
+			}
+			// Removes lower 1s
+			for(int k = i + 1; k < rows && matrix[k][j] != 0; k++)
+			{
+				addRowMultiple(k, i, -1);
+			}
+		}
+		
+	}
+		/*
+		1 1 1
+		2 0 0
+		2 0 2
+	
+		1 1 1
+		*/
+
+	/* **Getter and Setter Methods** */
 	public int getRows()
 	{
 		return rows;
@@ -63,12 +144,12 @@ public class Matrix
 		return cols;
 	}
 
-	public int[] getVals()
+	public float[] getVals()
 	{
 		return vals;
 	}
 
-	public int[][] getMatrix()
+	public float[][] getMatrix()
 	{
 		return matrix;
 	}
@@ -83,23 +164,25 @@ public class Matrix
 		cols = c;
 	}
 
-	public void setVals(int v[])
+	public void setVals(float v[])
 	{
 		vals = v;
 	}
 
-	public void setMatrix(int m[][])
+	public void setMatrix(float m[][])
 	{
 		matrix = m;
 	}
 
 	public static void main(String args[])
 	{
-		int[] y = {1, 2, 3, 4};
+		float[] y = {0, 0, 0, 1, 0, 1, 2, 1, 1, 0, 0, 0};
 		Matrix sand = new Matrix();
-		Matrix witch = new Matrix(2, 2, y);
+		Matrix witch = new Matrix(3, 4, y);
 		witch.fillMatrix();
 		sand.printMatrix();
+		witch.printMatrix();
+		witch.rowEchelon();
 		witch.printMatrix();
 		
 	}
